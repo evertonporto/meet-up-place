442eb0df3d6dc291b32b4005fd122dbb0af44165
Everton Porto <everton.porto@outlook.com>
2022-07-20 15:27:30 -0500

    Forms added, sending/fetching information added

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,52 @@
+import { useRef } from 'react'
+
+import Card from '../ui/Card'
+import classes from './NewMeetupForm.css'
+
+function NewMeetupForm(props) {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const adressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+        const enteredAdress = adressInputRef.current.value;
+
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAdress,
+            description: enteredDescription,
+        };
+        props.onAddMeetup(meetupData);
+    }
+    return (<Card>
+        <form className="form" onSubmit={submitHandler}>
+            <div className="control">
+                <label htmlFor="title">Meetup Title</label>
+                <input type="text" className="form-control" required id="title" ref={titleInputRef} />
+            </div> <div className="control">
+                <label htmlFor="image">Meetup Image</label>
+                <input type="url" className="form-control" required id="url" ref={imageInputRef} />
+            </div> <div className="control">
+                <label htmlFor="address">Meetup address</label>
+                <input type="text" className="form-control" required id="adress" ref={adressInputRef} />
+            </div>
+            <div className="control">
+                <label htmlFor="description">Description</label>
+                <textarea id="description" className="form-control" required rows='5' ref={descriptionInputRef}></textarea>
+            </div>
+            <div className="actions">
+                <button>Add Meetup</button>
+            </div>
+        </form>
+    </Card>
+    );
+
+}
+
+export default NewMeetupForm;
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,7 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetups() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-8a69b-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: { 'Content-Type': 'application/json' }
+            }
+        ).then(() => {
+            navigate('/')
+        });
+    }
+
     return (
         <div>
-            NewMeetups page
+            <h1>Add New Meetups</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </div>
     )
 }
